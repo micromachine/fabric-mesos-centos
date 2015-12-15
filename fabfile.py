682abf4278c06c5b6f9b246d776e471597ea2201
GKurkowiak <g.kurkowiak@gmail.com>
2015-12-15 11:13:42 +0100

    zookeeper config marathon config

--- fabfile.py
+++ fabfile.py
@@ -2,12 +2,12 @@
 from fabric.api import env,hosts,run,execute,roles
 from fabric.contrib.files import append
  
-env.hosts = ['192.168.122.7','192.168.122.65','192.168.122.53','192.168.122.216','192.168.122.40','192.168.122.244']
+env.hosts = ['mesos01','mesos02','mesos03','agent01','agent02','agent03']
 env.user = "root"
 env.password = "password"
 env.roledefs = {
-   'master': ['192.168.122.7','192.168.122.65','192.168.122.53'],
-   'slave': ['192.168.122.216','192.168.122.40','192.168.122.244']}
+   'master': ['mesos01','mesos02','mesos03'],
+   'slave': ['agent01','agent02','agent03']}
 def set_hosts():
 	run("rm -rf /etc/hosts")
         append('/etc/hosts', '127.0.0.1       localhost', use_sudo=True)
@@ -37,7 +37,6 @@ def restart_master_service():
 def restart_slave_service():
         run("service mesos-slave restart")
 	run("systemctl restart firewalld")
-
 @roles('master')
 def install_master():
 	run("yum -y install mesos marathon mesosphere-zookeeper")
@@ -48,18 +47,38 @@ def install_master():
 	run("firewall-cmd --zone=public --permanent --add-port=2888/tcp")	
 	run("firewall-cmd --zone=public --permanent --add-port=3888/tcp")	
 	run("firewall-cmd --zone=public --permanent --add-port=8080/tcp")	
+        run("firewall-cmd --zone=public --permanent --add-port=53/udp")
 	run("echo 2 > /etc/mesos-master/quorum")
-        if env.host == "mesos01": run("echo 1 > /var/lib/zookeeper/myid")
-        if env.host == "mesos02": run("echo 2 > /var/lib/zookeeper/myid")
-        if env.host == "mesos03": run("echo 3 > /var/lib/zookeeper/myid")
+        if env.host == "mesos01": 
+		run("echo 1 > /var/lib/zookeeper/myid")
+        if env.host == "mesos02": 
+		run("echo 2 > /var/lib/zookeeper/myid")
+        if env.host == "mesos03": 
+		run("echo 3 > /var/lib/zookeeper/myid")
 	run("rm -rf /etc/mesos/zk")
-	append('/etc/mesos/zk', 'zk://192.168.122.7:2181,192.168.122.65:2181,192.168.122.53:2181/mesos', use_sudo=True)
+	append('/etc/mesos/zk', 'zk://mesos01:2181,mesos02:2181,mesos03:2181/mesos', use_sudo=True)
+ 	append('/etc/zookeeper/conf/zoo.cfg', '# specify all zookeeper servers', use_sudo=True)
+        append('/etc/zookeeper/conf/zoo.cfg', 'server.1=mesos01:2888:3888', use_sudo=True)
+        append('/etc/zookeeper/conf/zoo.cfg', 'server.2=mesos03:2888:3888', use_sudo=True)
+        append('/etc/zookeeper/conf/zoo.cfg', 'server.3=mesos02:2888:3888', use_sudo=True)
+        if env.host == "mesos01":
+                run("echo 192.168.122.7 > /etc/mesos-master/ip")
+                run("cp /etc/mesos-master/ip /etc/mesos-master/hostname")
+        if env.host == "mesos02":
+                run("echo 192.168.122.65 > /etc/mesos-master/ip")
+                run("cp /etc/mesos-master/ip /etc/mesos-master/hostname")
+        if env.host == "mesos03":
+                run("echo 192.168.122.53 > /etc/mesos-master/ip")
+                run("cp /etc/mesos-master/ip /etc/mesos-master/hostname")
+	run("mkdir -p /etc/marathon/conf")
+        run("cp /etc/mesos-master/hostname /etc/marathon/conf")
+        run("cp /etc/mesos/zk /etc/marathon/conf/master")
+        run("cp /etc/marathon/conf/master /etc/marathon/conf/zk")
+        run("sed -i -e 's/mesos/marathon/g' /etc/marathon/conf/zk")
 	run("systemctl start zookeeper")
         run("service mesos-master restart")
         run("service marathon restart")
         run("systemctl restart firewalld")
-	run("echo master")
-
 @roles('slave')
 def install_agent():
 	run("yum -y install mesos")
@@ -68,8 +87,7 @@ def install_agent():
 	run("firewall-cmd --zone=public --permanent --add-port=5051/tcp")	
 	run("firewall-cmd --zone=public --permanent --add-port=2181/tcp")	
         run("rm -rf /etc/mesos/zk")
-        append('/etc/mesos/zk', 'zk://192.168.122.7:2181,192.168.122.65:2181,192.168.122.53:2181/mesos', use_sudo=True)
+        append('/etc/mesos/zk', 'zk://mesos01:2181,mesos02:2181,mesos03:2181/mesos', use_sudo=True)
         run("service mesos-slave restart")
         run("systemctl restart firewalld")
-	run("echo SLAVE")
 
